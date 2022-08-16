a4566b5fcc8d51a7ddabc7876d87f2423ce318ec
Alistair.Young <alistair.young@xero.com>
2022-08-16 12:15:28 +1200

    Updated with kkotlin sync

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -4,16 +4,10 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
 version = "2021.1"
 
-project {
-    description = "Maven 3.6 java project forked from anewtodolist"
-}
-
 project {                   
     description = "Maven 3.6 java project forked from anewtodolist"
     buildType(NewList_Build) 
 }
-
-
 
 object NewList_Build : BuildType({    
     id("Build")                       
