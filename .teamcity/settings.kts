c5ab471bc4b77992a36bf78e77bf4e34dd8e4cff
Alistair.Young <alistair.young@xero.com>
2022-08-23 06:50:02 +1200

    updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -6,3 +6,10 @@ version = "2021.1"
 project {
     subProject(MainProject())  // MainProject kts file must be named MainProject.kts
 }
+
+class MainProject() : Project() {
+    init {
+        name = "Main"
+        id(name.toId())
+    }
+}
