ff4e6840fa2591a898ec74815f9c9705b5aa030f
Alistair.Young <alistair.young@xero.com>
2022-08-22 21:21:40 +1200

    updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,4 +1,6 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
 version = "2021.1"
 
@@ -10,5 +12,28 @@ class MainProject() : Project() {
     init {
         name = "Main"
         id(name.toId())
+        buildType(Anothertodolist_Build)
     }
 }
+
+object Anothertodolist_Build : BuildType({
+    id("Build")
+    name = "Build"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        maven {
+            goals = "clean test"
+            runnerArgs = "-Dmaven.test.failure.ignore=true"
+            mavenVersion = bundled_3_6()
+        }
+    }
+
+    triggers {
+            vcs {
+            }
+    }
+})
