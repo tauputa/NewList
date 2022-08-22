c18756e206ab48edbca8ea360db6e03393dd99ea
Alistair.Young <alistair.young@xero.com>
2022-08-23 07:21:41 +1200

    updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -4,12 +4,10 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.project
 version = "2021.1"
 
 project {
-    subProject(MainProject())  // MainProject kts file must be named MainProject.kts
+    subProject(MainProject)  // MainProject kts file must be named MainProject.kts
 }
 
-class MainProject : Project() {
-    init {
-        name = "Main"
-        id(name.toId())
-    }
-}
+object MainProject : Project({ // singleton object is unique sub project
+    name = "Main"
+    id(name.toId())
+})
