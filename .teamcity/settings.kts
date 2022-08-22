2d141a9fc3f1d9d77d130301423248204e16ef86
Alistair.Young <alistair.young@xero.com>
2022-08-22 21:26:28 +1200

    updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -12,28 +12,46 @@ class MainProject() : Project() {
     init {
         name = "Main"
         id(name.toId())
-        buildType(Anothertodolist_Build)
+
+        val bts = sequential {
+            buildType(Maven("Build","clean compile","-Dmaven.test.failure.ignore=true"))
+            parallel{
+                buildType(Maven("Unit","clean test","-Dmaven.test.failure.ignore=true -Dtest=*.unit.*Test"))
+                buildType(Maven("Integration","clean test","-Dmaven.test.failure.ignore=true -Dtest=*.integration.*Test"))
+            }
+            buildType(Maven("Package","clean package","-Dmaven.test.failure.ignore=true -DskipTests"))
+        }.buildTypes()
+
+        bts.forEach{          // loop over the slice of maven class instances with forEach loops (forEach loops require it parameters)
+            buildType(it)
+        }
+
+//    for (i in bts) {    // this for loop also works nicely too
+//        buildType(i)
+//    }
+
+        bts.last().triggers{  // add vcs trigger for last element in list - ie package
+            vcs {
+            }
+        }
     }
 }
 
-object Anothertodolist_Build : BuildType({
-    id("Build")
-    name = "Build"
+class Maven(name:String,goals:String,runnerArgs:String?=null): BuildType({
+    id(name)            // use the name string variable as the unique build id - we can use another string variable though
 
+    this.name = name    // use the name string variable to populate the name used in this class
     vcs {
         root(DslContext.settingsRoot)
     }
-
     steps {
         maven {
-            goals = "clean test"
-            runnerArgs = "-Dmaven.test.failure.ignore=true"
+            this.goals = goals             // use the goals string var from the constructor to populate the goals member
+            this.runnerArgs = runnerArgs   // use the runnerArgs from the constructor to populate the runnerArgs member
             mavenVersion = bundled_3_6()
         }
     }
-
-    triggers {
-            vcs {
-            }
+    requirements {
+        contains("teamcity.agent.name", "linux")
     }
 })
