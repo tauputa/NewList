af300c2175a40d585144dd6462eb077a9cfe2448
Alistair.Young <alistair.young@xero.com>
2022-08-24 08:26:41 +1200

    updated

--- .teamcity/boggle.kt
+++ .teamcity/boggle.kt
@@ -1,7 +1,8 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 
 object SubProjectOne : Project({ // singleton object is unique sub project
-    name = "SubProjectOne"
+    name = "Boggle"
+    //name = "SubProjectOne"
     id(name.toId())
     subProject(SubProjectTwo)    // contains subproject
 })
