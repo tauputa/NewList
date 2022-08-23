eb9727e00952ffd9f77a75de3757bf4684213973
Alistair.Young <alistair.young@xero.com>
2022-08-24 07:51:26 +1200

    updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -4,7 +4,16 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.project
 version = "2021.1"
 
 project {
-    subProject(SubProjectOne())     // instantiate SubProjectOne class with default constructor
-}
+    subProject(SubProjectOne)    // contains subproject
+    }
 
+object SubProjectOne : Project({ // singleton object is unique sub project
+    name = "SubProjectOne"
+    id(name.toId())
+    subProject(SubProjectTwo)    // contains subproject
+})
 
+object SubProjectTwo : Project({ // singleton object is unique sub project
+    name = "SubProjectTwo"
+    id(name.toId())
+})
