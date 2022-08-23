4760f6986654a6c3cfcf078a1b04085adabf6210
Alistair.Young <alistair.young@xero.com>
2022-08-24 08:03:01 +1200

    updated

--- .teamcity/SubProjectOne.kt
+++ .teamcity/SubProjectOne.kt
@@ -1,5 +1,4 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
-import jetbrains.buildServer.configs.kotlin.v2019_2.project
 
 object SubProjectOne : Project({ // singleton object is unique sub project
     name = "SubProjectOne"
