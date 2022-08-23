6b263aeca3485f7be3df1c84f80fd4abe8982d82
Alistair.Young <alistair.young@xero.com>
2022-08-23 20:21:37 +1200

    updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -6,18 +6,5 @@ version = "2021.1"
 project {
     subProject(SubProjectOne())     // instantiate SubProjectOne class with default constructor
     }
-
-class SubProjectOne : Project() {   // derived class for SubProjectOne must call base class constructor
-    init {
-        name = "SubProjectOne"
-        id(name.toId())
-        subProject(SubProjectTwo()) // instantiate SubProjectTwo class with default constructor
-    }    
 }
 
-class SubProjectTwo : Project() {   // derived class for SubProjectTwo must call base class constructor
-    init {
-        name = "SubProjectTwo"
-        id(name.toId())
-    }
-}
