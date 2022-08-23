ccda6b419750615902ad7a3137f429419dede7a2
Alistair.Young <alistair.young@xero.com>
2022-08-23 20:25:35 +1200

    updated

--- .teamcity/SubProjectTwo.kt
+++ .teamcity/SubProjectTwo.kt
@@ -1,8 +1,6 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.project
 
-version = "2021.1"
-
 class SubProjectTwo : Project() {   // derived class for SubProjectTwo must call base class constructor
     init {
         name = "SubProjectTwo"
